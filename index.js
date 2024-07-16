2d11e3207976c29ae26d23469c6db568a17485c3
Abhay <abhaysharma.developer@gmail.com>
2024-07-17 00:01:19 +0530

    Demo version done

---
+++ index.js
@@ -0,0 +1,221 @@
+let gridArray = [
+  [
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
+  ],
+];
+
+const container = document.querySelector(".grid-container");
+const nextButton = document.querySelector(".next-btn");
+const totalBoxes = [].concat(...gridArray).length;
+
+// To display the array on the browser in form of a grid
+const displayGrid = () => {
+  const grid = gridArray.map((rows, index1) => {
+    return `<div>
+        ${rows.map((item, index2) => {
+          return `<div  data-index1="${index1}" data-index2="${index2}" class='box' style="background-color: ${
+            item == 0 ? "gray" : "yellow"
+          }">${index1} ${index2} </div>`;
+        })}
+            </div>`;
+  });
+  container.innerHTML = grid.join().replace(/\,/g, "");
+
+  const box = document.querySelectorAll(".box");
+  for (let i = 0; i < totalBoxes; i++) {
+    box[i].addEventListener("click", (e) => changeState(e));
+  }
+};
+
+// To change the state of individual box on click
+const changeState = (e) => {
+  const ind1 = e.target.dataset.index1;
+  const ind2 = e.target.dataset.index2;
+
+  gridArray[ind1][ind2] === 0
+    ? (gridArray[ind1][ind2] = 1)
+    : (gridArray[ind1][ind2] = 0);
+
+  displayGrid();
+};
+
+//To change state of entire grid
+const nextState = () => {
+  const newGrid = gridArray.map((row) => [...row]);
+  const box = document.querySelectorAll(".box");
+  let index1;
+  let index2;
+
+  for (let i = 0; i < totalBoxes; i++) {
+    index1 = Number(box[i].dataset.index1);
+    index2 = Number(box[i].dataset.index2);
+
+    const initialState = gridArray[index1][index2];
+    let count = 0;
+
+    if (index2 + 1 < gridArray.length && gridArray[index1][index2 + 1]) count++;
+    if (index2 > 0 && gridArray[index1][index2 - 1]) count++;
+    if (index1 + 1 < gridArray.length && gridArray[index1 + 1][index2]) count++;
+    if (
+      index1 + 1 < gridArray.length &&
+      index2 + 1 < gridArray.length &&
+      gridArray[index1 + 1][index2 + 1]
+    )
+      count++;
+    if (
+      index1 + 1 < gridArray.length &&
+      index2 > 0 &&
+      gridArray[index1 + 1][index2 - 1]
+    )
+      count++;
+    if (index1 > 0 && gridArray[index1 - 1][index2]) count++;
+    if (
+      index1 > 0 &&
+      index2 + 1 < gridArray.length &&
+      gridArray[index1 - 1][index2 + 1]
+    )
+      count++;
+    if (index1 > 0 && index2 > 0 && gridArray[index1 - 1][index2 - 1]) count++;
+
+    if (initialState == 0 && count == 3) {
+      newGrid[index1][index2] = 1;
+    }
+
+    if (
+      (initialState == 1 && count >= 4) ||
+      (initialState == 1 && count <= 1)
+    ) {
+      newGrid[index1][index2] = 0;
+    }
+  }
+
+  gridArray = newGrid.map((row) => [...row]);
+  displayGrid();
+};
+
+nextButton.addEventListener("click", nextState);
+
+//Displays the intial state
+displayGrid();
